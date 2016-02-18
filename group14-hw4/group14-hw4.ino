f2005b602dc461cca19eb05411a004c5f33b1ceb
adamclaassen <claassenadam@gmail.com>
2016-02-18 11:31:15 -0600

    base code

--- group14-hw4/group14-hw4.ino
+++ group14-hw4/group14-hw4.ino
@@ -5,3 +5,16 @@
  * Tianchin Fu
  * Luke Hurtig
  */
+
+int sensorPin = A0;
+int measureDelay = 20;
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  Serial.print(analogRead(A0));
+  delay(measureDelay);
+}
+
