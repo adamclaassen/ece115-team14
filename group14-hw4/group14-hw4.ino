5af60f284cedbf564f93fc87a064ef3c63409c64
adamclaassen <claassenadam@gmail.com>
2016-02-22 23:26:12 -0600

    added code for distance conversion based on the V vs. 1/L graph in sensor datasheet

--- group14-hw4/group14-hw4.ino
+++ group14-hw4/group14-hw4.ino
@@ -8,21 +8,31 @@
 
 int sensorPin = A0;
 int measureDelay = 20;
-double distConst = (450.0/1024.0);
-int distOffset = 100;
+
 
 void setup(){
   Serial.begin(9600);
 }
 
 void loop(){
-  sensorRead();
-}
-
-void sensorRead(){
-  Serial.print((analogRead(A0)*distConst)+distOffset);
+  Serial.print(sensorTransferFunc(sensorRead()));
   Serial.println(" cm");
   delay(measureDelay);
 }
 
+int sensorRead(){
+  double adcConst = 0.00459; //4.7v/1024 divisions
+  double voltage = adcConst*analogRead(A0);
+  if(voltage<1.4){voltage=-1;}//out of range under
+  if(voltage>2.5){voltage=-2;}// out of range over
+}
 
+double sensorTransferFunc(double input){ //voltage -> distance conversion
+  if(!input<0){
+  double slope = 0.0072727272; //calculated from sensor datasheet V vs. 1/L graph
+  double offset = -0.00818181; //calculated from sensor datasheet V vs. 1/L graph
+  double invDist = (input*slope)+offset;
+  return 1/invDist;
+  }
+  return input;
+}
