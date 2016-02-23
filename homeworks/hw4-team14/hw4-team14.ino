cd3d639035056d4dccc0a100185dce6c8846e1bd
adamclaassen <claassenadam@gmail.com>
2016-02-22 23:48:02 -0600

    changed file structure, added HW4 that matches the requirements, moved more complete distance sensor code to an new file

---
+++ homeworks/hw4-team14/hw4-team14.ino
@@ -0,0 +1,30 @@
+/*
+ * Team 14 Homework 4
+ * Adam Becker
+ * Adam Claassen
+ * Tianchin Fu
+ * Luke Hurtig
+ * 
+ * Program revision 1
+ * Due 2016-2-23
+ * 
+ * This program aquires a distance measurement from the IR sensor and prints it over serial every 20ms (blocking). 
+ */
+
+int sensorPin = A0;
+int measureDelay = 20;
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  int analogValue = sensorRead();
+  Serial.println(analogValue);
+  delay(measureDelay);
+}
+
+int sensorRead(){
+  analogRead(sensorPin);
+}
+
